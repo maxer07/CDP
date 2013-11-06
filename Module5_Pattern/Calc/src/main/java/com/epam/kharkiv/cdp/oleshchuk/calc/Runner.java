daaeb77e9ee32df66623c03648d32e8f7a920330
Maksym_Oleshchuk <maxer07@mail.ua>
2013-11-06 18:57:37 +0200

    fix2 calc

--- Module5_Pattern/Calc/src/main/java/com/epam/kharkiv/cdp/oleshchuk/calc/Runner.java
+++ Module5_Pattern/Calc/src/main/java/com/epam/kharkiv/cdp/oleshchuk/calc/Runner.java
@@ -9,7 +9,8 @@ public class Runner {
     public static void main(String[] args) {
         try{
         final String input = System.getenv("input");
-        String result = ShuntingYard.calculate(input);
+        String prepareString = StringUtil.prepareInputString(input);
+        String result = ShuntingYard.calculate(prepareString);
         System.out.println(result);
         } catch (Exception e) {
             System.out.println("FAIL");
